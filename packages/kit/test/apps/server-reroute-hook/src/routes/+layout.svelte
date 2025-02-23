e6d66c47b52c8860847f09a026b4ab174bf2bfaf
Nikolaos Grammatikos <nikosgram@oglofus.com>
2025-02-23 00:17:05 +0000

    feat: add server reroute hook

---
+++ packages/kit/test/apps/server-reroute-hook/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { setup } from '../../../../setup.js';
+
+	setup();
+
+	const { children } = $props();
+</script>
+
+<a href="/">/</a>
+<a href="/reroute">/reroute</a>
+<a href="/test">/test</a>
+<a href="/somewhere">/somewhere</a>
+<a href="/not-rerouted">/not-rerouted</a>
+
+{@render children()}
